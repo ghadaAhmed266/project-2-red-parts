ba1c206c2b581ecf64717335a82213d091f8104f
ghada ahmed <ghadaahmed2019266@gmail.com>
2023-01-09 06:08:06 +0200

    add all files to red-parts  repo

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@popperjs/core";
+import "bootstrap";
+import '../scss/app.scss';
+/* Your JS Code goes here */
+
+/* Demo JS 
+import './demo.js';
+$(function() {
+    $(".carousel-img").height=$(".slider-content").innerHeight;
+});*/
